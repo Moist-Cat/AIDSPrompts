d4018bf2f010eb14eb16bd25cc1748cdf0e2573c
NeoClubAIDS <108577084+NeoClubAIDS@users.noreply.github.com>
2022-07-18 15:01:09 +0200

    Update form.js
    Use Json.parse instead of regex.

--- src/js/form.js
+++ src/js/form.js
@@ -92,30 +92,30 @@
                 var file = fileInput.files[0];
                 var lengthmax = 0;
                 var reader = new FileReader();
+                var ftags = "";
+                var WIkeys;
+                var i;
+                var j;
+
                 reader.onload = function (e) {
 
                     var str = reader.result.replace(/[\r\n]/gm, '');
 
                     $("#Command_File").val((str));
-                    var i;
+                    const fscenario = JSON.parse(str);
 
+                    $("#Command_Title").val(fscenario.title.replace(/\\n/g, '\n').replace(/\\/g, '').replace(/""/g, '"'));
+                    $("#Command_PromptContent").val(fscenario.prompt.replace(/\\n/g, '\n').replace(/\\/g, '').replace(/""/g, '"'));
+                    $("#Command_Pf").val(fscenario.prompt);
+                    $("#Command_Description").val(fscenario.description.replace(/\\/g, '').replace(/""/g, '"'));
+                    for (let i in fscenario.tags)
+                        ftags += fscenario.tags[i] + ",";
+                    ftags = ftags.slice(0, ftags.length - 1)
+                    $("#Command_PromptTags").val(ftags.replace(/\\n/g, '\n').replace(/\\/g, '').replace(/\"/g, "").replace(/\s+/g, " "));
+                    $("#Command_Memory").val(fscenario.context[0].text.replace(/\\n/g, '\n').replace(/\\/g, '').replace(/""/g, '"'));
+                    $("#Command_AuthorsNote").val(fscenario.context[1].text.replace(/\\n/g, '\n').replace(/\\/g, '').replace(/""/g, '"'));
 
-                    // I hate Regex so much it's unreal.
-                    $("#Command_Title").val((str.match(/(?<="title": *").*?(?=", *")/g)[0]).replace(/\\n/g, '\n').replace(/\\/g, '').replace(/""/g, '"'));
-                    $("#Command_PromptContent").val((str.match(/(?<="prompt": *").*?(?=", *")/g)[0]).replace(/\\n/g, '\n').replace(/\\/g, '').replace(/""/g, '"'));
-                    $("#Command_Pf").val(str.match(/(?<="prompt": *").*?(?=", *")/g)[0]);
-                    $("#Command_Description").val((str.match(/(?<="description": *").*?(?=", *")/g)[0]).replace(/\\n/g, '\n').replace(/\\/g, '').replace(/""/g, '"'));
-                    $("#Command_PromptTags").val((str.match(/(?<="tags": *\[).*?(?=] *)/g)[0]).replace(/\\n/g, '\n').replace(/\\/g, '').replace(/\"/g, "").replace(/\s+/g, " "));
-                    $("#Command_Memory").val((str.match(/(?<="context": *\[ *{ *"text": *").*?(?=" *, *")/g)[0]).replace(/\\n/g, '\n').replace(/\\/g, '').replace(/""/g, '"'));
-                    $("#Command_AuthorsNote").val((str.match(/(?<=}, *{ *"text": *").*?(?=" *, *")/g)[0]).replace(/\\n/g, '\n').replace(/\\/g, '').replace(/""/g, '"'));
-
-                    var str2 = str.match(/(?<="entries": *\[ *{ *").*?(?=]})/g);
-                    if (str2 != null) {
-                        str2 = str2[0];
-                        lengthmax = str2.match(/(?<="keys": *\[).*?(?=], *")/g).length;
-                    }
-
-
+                    lengthmax = fscenario.lorebook.entries.length;
                     var p = counterWI;
 
                     //We remove World info div if too many on the page when compared to number of WI in the file.
@@ -126,17 +126,19 @@
                         $("#br" + i).remove();
 
                     }
+
                     $("#Command_CWI").attr("value", counterWI);
 
-                    for (i = 0; i < lengthmax; i++) {
-
-
+                    for (let i in fscenario.lorebook.entries) {
+                        WIkeys = "";
                         $('#add-wi').trigger('click');
-                        var str2 = str.match(/(?<="entries": *\[ *{ *").*?(?=]})/g)[0];
-                        // We give Key and Description the value in the file
-                        $("#Command__WI" + i).val((str2.match(/(?<=text": *).*?(?=" *, *")/g)[i]).replace(/\\n/g, '\n').replace(/\\/g, '').replace(/\"/g, "").trim());
 
-                        $("#Command__WIK" + i).val((str2.match(/(?<="keys": *\[ *" *).*?(?=], *")/g)[i]).replace(/\\n/g, '\n').replace(/\\/g, '').replace(/\"/g, "").replace(/\s+/g, " "));
+                        // We give Key and Description the value in the file
+                        $("#Command__WI" + i).val(fscenario.lorebook.entries[i].text.replace(/\\n/g, '\n').replace(/\\/g, '').replace(/\"/g, "").trim());
+                        for (let j in fscenario.lorebook.entries[i].keys)
+                            WIkeys += fscenario.lorebook.entries[i].keys[j] + ",";
+                        WIkeys = WIkeys.slice(0, WIkeys.length - 1)
+                        $("#Command__WIK" + i).val(WIkeys.replace(/\\n/g, '\n').replace(/\\/g, '').replace(/\"/g, "").replace(/\s+/g, " "));
 
 
                     }
